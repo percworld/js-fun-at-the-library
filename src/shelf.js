6bf6cf13d913fc05e88ec506b3d3a47829259011
Charles Morris <percworld@gmail.com>
2020-12-06 11:25:38 -0700

    Add shelving functions

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,43 @@
 
+function shelfBook (book, shelf) {
+  if (shelf.length <= 2) {
+    shelf.unshift(book);
+  };
+  return shelf;
+};
+
+function unshelfBook(title, shelf) {
+  shelf.splice(1,1);
+  return shelf;
+};
+
+function listTitles(shelf) {
+  var titles = '';
+  for (var i = 0; i < shelf.length; i++) {
+     if (i == shelf.length - 1) {
+      titles = titles + shelf[i]['title'];
+    } else {
+      titles = titles + shelf[i]['title'] + ', ';
+    };
+  };
+  return titles;
+};
+
+function searchShelf(shelf, title) {
+  var available;
+  for (var i = 0; i < shelf.length; i++) {
+    if (shelf[i]['title'] == title) {
+      available = true;
+    } else {
+      available = false;
+    };
+  };
+  return available;
+};
+
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf
 };
